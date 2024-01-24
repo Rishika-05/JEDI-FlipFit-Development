b8392db2b564980b19ea40c6d803ed4b17a20d81
Prathamesh <prathamesh.parit@flipkart.com>
2024-01-24 11:35:33 +0530

    chore: beutify

--- JEDI-Flipfit-JAVA-POS/src/com/flipkart/bean/GymOwner.java
+++ JEDI-Flipfit-JAVA-POS/src/com/flipkart/bean/GymOwner.java
@@ -8,7 +8,8 @@ package com.flipkart.bean;
  */
 public class GymOwner extends User {
     private String aadhar;
-    private String bank_account;
+    private String bankAccount;
+
     public String getAadhar() {
         return aadhar;
     }
@@ -17,16 +18,11 @@ public class GymOwner extends User {
         this.aadhar = aadhar;
     }
 
-    public String getBank_account() {
-        return bank_account;
+    public String getBankAccount() {
+        return bankAccount;
     }
 
-    public void setBank_account(String bank_account) {
-        this.bank_account = bank_account;
+    public void setBankAccount(String bankAccount) {
+        this.bankAccount = bankAccount;
     }
-
-
-
-
-
 }

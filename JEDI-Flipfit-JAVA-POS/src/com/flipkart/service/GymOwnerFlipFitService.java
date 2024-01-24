e06c5521141ac77df3818bf793b454efe331db9a
sakshi-s0ni <sakshisoni61000@gmail.com>
2024-01-24 21:58:34 +0530

    GymOwner Services

--- JEDI-Flipfit-JAVA-POS/src/com/flipkart/service/GymOwnerFlipFitService.java
+++ JEDI-Flipfit-JAVA-POS/src/com/flipkart/service/GymOwnerFlipFitService.java
@@ -3,9 +3,17 @@ package com.flipkart.service;
 import java.util.List;
 
 import com.flipkart.bean.Gym;
+import com.flipkart.bean.GymOwner;
+import com.flipkart.bean.User;
 
 public interface GymOwnerFlipFitService {
-	void addGym(Gym gmy);
 	
+	void addGym(Gym gym);
 	List<Gym> getAllGyms(int userId);
+	
+	void addTimeSlot(Gym gym);
+	
+	GymOwner viewProfile(User user);
+	GymOwner updateProfile(GymOwner owner);
+	
 }
--- JEDI-Flipfit-JAVA-POS/src/com/flipkart/service/serviceImpl/GymOwnerFlipFitServiceImpl.java
+++ JEDI-Flipfit-JAVA-POS/src/com/flipkart/service/serviceImpl/GymOwnerFlipFitServiceImpl.java
@@ -6,6 +6,8 @@ package com.flipkart.service.serviceImpl;
 import java.util.List;
 
 import com.flipkart.bean.Gym;
+import com.flipkart.bean.GymOwner;
+import com.flipkart.bean.User;
 import com.flipkart.service.GymOwnerFlipFitService;
 
 /**
@@ -24,5 +26,26 @@ public class GymOwnerFlipFitServiceImpl implements GymOwnerFlipFitService {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	@Override
+	public void addTimeSlot(Gym gym) {
+		//logic to add time slot
+		return;
+	}
+	
 
+	
+	@Override
+	public GymOwner viewProfile(User user) {
+		//Get gym owner profile from username and password
+		
+		GymOwner gymOwnerProfile = new GymOwner();
+		return gymOwnerProfile;
+	}
+	
+	@Override
+	public GymOwner updateProfile(GymOwner gymOwner) {
+		//get details and update accordingly
+		
+		return gymOwner;
+	}
 }

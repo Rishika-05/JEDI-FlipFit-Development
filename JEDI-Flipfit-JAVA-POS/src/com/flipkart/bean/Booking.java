ecddcca653bff3e67684cc4d7f83a84996cc5f77
Prathamesh Parit <prathamesh.parit@blretv-c02z53l2.fkipl.flipkart.com>
2024-01-24 10:34:51 +0530

    feat: added waitlist attributes

--- JEDI-Flipfit-JAVA-POS/src/com/flipkart/bean/Booking.java
+++ JEDI-Flipfit-JAVA-POS/src/com/flipkart/bean/Booking.java
@@ -8,4 +8,6 @@ package com.flipkart.bean;
  */
 public class Booking {
 
+
+
 }
--- JEDI-Flipfit-JAVA-POS/src/com/flipkart/bean/Waitlist.java
+++ JEDI-Flipfit-JAVA-POS/src/com/flipkart/bean/Waitlist.java
@@ -1,5 +1,51 @@
 package com.flipkart.bean;
-
+import java.time.LocalDateTime
 public class Waitlist {
+    private int id;
+    private int user_id;
+    private int slot_id;
+    private LocalDateTime booking_date;
+    private boolean is_cancelled;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getSlot_id() {
+        return slot_id;
+    }
+
+    public void setSlot_id(int slot_id) {
+        this.slot_id = slot_id;
+    }
+
+    public LocalDateTime getBooking_date() {
+        return booking_date;
+    }
+
+    public void setBooking_date(LocalDateTime booking_date) {
+        this.booking_date = booking_date;
+    }
+
+    public boolean isIs_cancelled() {
+        return is_cancelled;
+    }
+
+    public void setIs_cancelled(boolean is_cancelled) {
+        this.is_cancelled = is_cancelled;
+    }
+
+
 
 }

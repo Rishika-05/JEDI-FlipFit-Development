2ccb2a9931bc9e9026ad8a77a43c32116308410c
Rishika-05 <rishikaraj7263@gmail.com>
2024-01-23 21:15:35 -0800

    Feat: update constant & enum

--- JEDI-Flipfit-JAVA-POS/src/com/flipkart/constant/Gender.java
+++ JEDI-Flipfit-JAVA-POS/src/com/flipkart/constant/Gender.java
@@ -7,5 +7,15 @@ package com.flipkart.constant;
  * 
  */
 public enum Gender {
-
+	
+	MALE("Male"),
+	FEMALE("Female"),
+	OTHER("Other");
+	
+	
+	private Gender(String gender) {
+		this.gender = gender;
+	}
+	
+	private final String gender;
 }
--- JEDI-Flipfit-JAVA-POS/src/com/flipkart/constant/Role.java
+++ JEDI-Flipfit-JAVA-POS/src/com/flipkart/constant/Role.java
@@ -7,5 +7,14 @@ package com.flipkart.constant;
  * 
  */
 public enum Role {
-
+	
+	ADMIN("Admin"),
+	GYM_CUSTOMER("Gym Customer"),
+	GYM_OWNER("Gym Owner");
+	
+	private Role(String role) {
+		this.role = role;
+	}
+	
+	private final String role;
 }

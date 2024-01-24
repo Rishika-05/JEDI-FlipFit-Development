943531c263218b8f623546831f091db6e02d39ca
Prathamesh <prathamesh.parit@flipkart.com>
2024-01-24 14:36:02 +0530

    fix: bug fixes

--- JEDI-Flipfit-JAVA-POS/src/com/flipkart/service/GymFlipFitService.java
+++ JEDI-Flipfit-JAVA-POS/src/com/flipkart/service/GymFlipFitService.java
@@ -3,9 +3,15 @@ package com.flipkart.service;
 import java.util.List;
 
 import com.flipkart.bean.Slot;
+import com.flipkart.bean.GymDetails;  // Add this import statement
 
 public interface GymFlipFitService {
 	List<Slot> getSlots(int userId);
+<<<<<<< HEAD
 	
 //	GymDetails getGymDetails(int gymId);
+=======
+
+	GymDetails getGymDetails(int gymId);
+>>>>>>> 4eefed4 (fix: bug fixes)
 }
--- JEDI-Flipfit-JAVA-POS/src/com/flipkart/service/serviceImpl/SlotFlipFitServiceImpl.java
+++ JEDI-Flipfit-JAVA-POS/src/com/flipkart/service/serviceImpl/SlotFlipFitServiceImpl.java
@@ -10,10 +10,17 @@ import com.flipkart.service.SlotFlipFitService;
  */
 public class SlotFlipFitServiceImpl implements SlotFlipFitService {
 
+<<<<<<< HEAD
 	@Override
 	public boolean isAvailable() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+=======
+    @Override
+    public boolean isAvailable() {
+        return false;
+    }
+>>>>>>> 4eefed4 (fix: bug fixes)
 }

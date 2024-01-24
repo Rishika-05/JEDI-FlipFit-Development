1a46176f19cac9e8d914ffe017a882aaed121e68
Prathamesh <prathamesh.parit@flipkart.com>
2024-01-24 14:22:45 +0530

    fix: bug fixes

--- JEDI-Flipfit-JAVA-POS/src/com/flipkart/service/SlotFlipFitService.java
+++ JEDI-Flipfit-JAVA-POS/src/com/flipkart/service/SlotFlipFitService.java
@@ -1,15 +1,5 @@
 package com.flipkart.service;
 
-<<<<<<< HEAD:JEDI-Flipfit-JAVA-POS/src/com/flipkart/service/SlotFlipFitService.java
 public interface SlotFlipFitService {
-<<<<<<< HEAD
-
 	boolean isAvailable();
-=======
-	boolean isAvailable()
-=======
-public interface SlotService {
-	boolean isAvailable();
->>>>>>> ac97f8a (feat: created gym customer menu options):JEDI-Flipfit-JAVA-POS/src/com/flipkart/service/SlotService.java
->>>>>>> 0af6dc8 (fix: bug fix)
 }

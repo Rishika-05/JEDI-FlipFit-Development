f3a97f9acd5a8bf868dc9662fc84d11be1e17423
sakshi-s0ni <sakshisoni61000@gmail.com>
2024-01-24 22:23:08 +0530

    Slots Service

--- JEDI-Flipfit-JAVA-POS/src/com/flipkart/client/GymOwnerFlipFitMenu.java
+++ JEDI-Flipfit-JAVA-POS/src/com/flipkart/client/GymOwnerFlipFitMenu.java
@@ -64,14 +64,10 @@ public class GymOwnerFlipFitMenu {
                     System.out.println("You have selected an invalid option. Please try again!!");
                     break;
             }
-<<<<<<< HEAD
 
-=======
->>>>>>> 2e7ce98 (fixed bugs)
         } while (menuOption != 11);
 
     }
-
 
     private void addNewGym() {
         // Implement logic to add a new gym center
--- JEDI-Flipfit-JAVA-POS/src/com/flipkart/service/serviceImpl/SlotFlipFitServiceImpl.java
+++ JEDI-Flipfit-JAVA-POS/src/com/flipkart/service/serviceImpl/SlotFlipFitServiceImpl.java
@@ -3,6 +3,9 @@
  */
 package com.flipkart.service.serviceImpl;
 
+import java.util.ArrayList;
+
+import com.flipkart.bean.Slot;
 import com.flipkart.service.SlotFlipFitService;
 
 /**
@@ -14,4 +17,24 @@ public class SlotFlipFitServiceImpl implements SlotFlipFitService {
     public boolean isAvailable() {
         return false;
     }
+    
+  
+
+	@Override
+	public boolean addSlot(int slotHour, int gymID, int availableSeats) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean updateSlot(int slotHour, int gymID, int changeInSeats) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
+	@Override
+	public ArrayList<Slot> getAllAvailableSlots() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }

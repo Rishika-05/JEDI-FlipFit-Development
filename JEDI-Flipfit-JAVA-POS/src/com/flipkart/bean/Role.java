d4637a27c454f866ef3d072d69d8444001266e94
deepakguptaflipkart <deepak.vinodgupta@flipkart.com>
2024-01-24 11:26:31 +0530

    feat: added role bean

---
+++ JEDI-Flipfit-JAVA-POS/src/com/flipkart/bean/Role.java
@@ -0,0 +1,23 @@
+package com.flipkart.bean;
+
+public class Role {
+    private int userId;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    private String role;
+}

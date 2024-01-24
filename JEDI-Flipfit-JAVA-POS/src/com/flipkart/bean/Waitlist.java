4844e6acab16b91269eae5c4af4e862f362f844d
Prathamesh <prathamesh.parit@flipkart.com>
2024-01-24 10:44:14 +0530

    fix: naming conventions corrected

--- JEDI-Flipfit-JAVA-POS/src/com/flipkart/bean/Waitlist.java
+++ JEDI-Flipfit-JAVA-POS/src/com/flipkart/bean/Waitlist.java
@@ -3,10 +3,11 @@ import java.time.LocalDateTime;
 
 public class Waitlist {
     private int id;
-    private int user_id;
-    private int slot_id;
-    private LocalDateTime booking_date;
-    private boolean is_cancelled;
+    private int userId;
+    private int slotId;
+    private LocalDateTime bookingDate = LocalDateTime.now();
+    private boolean isCancelled;
+
     public int getId() {
         return id;
     }
@@ -15,38 +16,35 @@ public class Waitlist {
         this.id = id;
     }
 
-    public int getUser_id() {
-        return user_id;
+    public int getUserId() {
+        return userId;
     }
 
-    public void setUser_id(int user_id) {
-        this.user_id = user_id;
+    public void setUserId(int userId) {
+        this.userId = userId;
     }
 
-    public int getSlot_id() {
-        return slot_id;
+    public int getSlotId() {
+        return slotId;
     }
 
-    public void setSlot_id(int slot_id) {
-        this.slot_id = slot_id;
+    public void setSlotId(int slotId) {
+        this.slotId = slotId;
     }
 
-    public LocalDateTime getBooking_date() {
-        return booking_date;
+    public LocalDateTime getBookingDate() {
+        return bookingDate;
     }
 
-    public void setBooking_date(LocalDateTime booking_date) {
-        this.booking_date = booking_date;
+    public void setBookingDate(LocalDateTime bookingDate) {
+        this.bookingDate = bookingDate;
     }
 
-    public boolean isIs_cancelled() {
-        return is_cancelled;
+    public boolean isCancelled() {
+        return isCancelled;
     }
 
-    public void setIs_cancelled(boolean is_cancelled) {
-        this.is_cancelled = is_cancelled;
+    public void setCancelled(boolean cancelled) {
+        isCancelled = cancelled;
     }
-
-
-
 }

34b63f917809a9e0584ceef653e6bcac4b37b2fa
rktejesh <rk.tejesh@gmail.com>
2024-01-24 11:02:40 +0530

    Added service interface functions

--- JEDI-Flipfit-JAVA-POS/src/com/flipkart/service/BookingService.java
+++ JEDI-Flipfit-JAVA-POS/src/com/flipkart/service/BookingService.java
@@ -1,5 +1,8 @@
 package com.flipkart.service;
 
 public interface BookingService {
-
+	
+	boolean updateBooking(int bookingId);
+	
+	boolean cancelBooking(int bookingId);
 }
---
+++ JEDI-Flipfit-JAVA-POS/src/com/flipkart/service/GymAdminService.java
@@ -0,0 +1,24 @@
+package com.flipkart.service;
+
+import java.util.List;
+
+import com.flipkart.bean.Gym;
+import com.flipkart.bean.GymCustomer;
+import com.flipkart.bean.GymOwner;
+
+public interface GymAdminService {
+	
+	GymOwner verifyGymOwnerRequest(int gym_owner_id, boolean approved);
+	
+	void approveGymRequest(int gym_id);
+	
+	void rejectGymRequest(int gym_id);
+	
+	List<Gym> getGymRequests();
+	
+	List<GymOwner> getGymOwnerRequests();
+	
+	List<GymCustomer> getAllGymCustomers();
+	
+	List<GymOwner> getAllGymOwners();
+}

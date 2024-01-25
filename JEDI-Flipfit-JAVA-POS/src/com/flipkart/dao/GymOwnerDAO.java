b9ac862a5822844badafaa7a4b96b210b6675166
sakshi-s0ni <sakshisoni61000@gmail.com>
2024-01-25 09:54:29 +0530

    GymOwner Dao

--- JEDI-Flipfit-JAVA-POS/src/com/flipkart/dao/GymOwnerDAO.java
+++ JEDI-Flipfit-JAVA-POS/src/com/flipkart/dao/GymOwnerDAO.java
@@ -3,9 +3,21 @@
  */
 package com.flipkart.dao;
 
+import com.flipkart.bean.Gym;
+import com.flipkart.bean.GymOwner;
+import com.flipkart.bean.User;
+
+import java.util.List;
+
 /**
  * 
  */
 public interface GymOwnerDAO {
+    void addGym(Gym gym);
+    List<Gym> getAllGyms(int userId);
 
+    void addTimeSlot(Gym gym);
+
+    GymOwner viewProfile(User user);
+    GymOwner updateProfile(GymOwner owner);
 }
--- JEDI-Flipfit-JAVA-POS/src/com/flipkart/dao/daoImpl/GymOwnerDAOImpl.java
+++ JEDI-Flipfit-JAVA-POS/src/com/flipkart/dao/daoImpl/GymOwnerDAOImpl.java
@@ -3,9 +3,61 @@
  */
 package com.flipkart.dao.daoImpl;
 
+import com.flipkart.bean.Gym;
+import com.flipkart.bean.GymOwner;
+import com.flipkart.bean.User;
+import com.flipkart.dao.GymOwnerDAO;
+
+import java.util.List;
+
+
 /**
  * 
  */
-public class GymOwnerDAOImpl {
+public class GymOwnerDAOImpl implements GymOwnerDAO {
 
+    private static GymOwnerDAOImpl gymOwnerDaoObj = null;
+
+    private GymOwnerDAOImpl() {
+    }
+
+    public static synchronized GymOwnerDAOImpl getInstance() {
+        if (gymOwnerDaoObj == null)
+            gymOwnerDaoObj = new GymOwnerDAOImpl();
+
+        return gymOwnerDaoObj;
+    }
+    @Override
+    public void addGym(Gym gmy) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public List<Gym> getAllGyms(int userId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    @Override
+    public void addTimeSlot(Gym gym) {
+        //logic to add time slot
+        return;
+    }
+
+
+
+    @Override
+    public GymOwner viewProfile(User user) {
+        //Get gym owner profile from username and password
+
+        GymOwner gymOwnerProfile = new GymOwner();
+        return gymOwnerProfile;
+    }
+
+    @Override
+    public GymOwner updateProfile(GymOwner gymOwner) {
+        //get details and update accordingly
+
+        return gymOwner;
+    }
 }

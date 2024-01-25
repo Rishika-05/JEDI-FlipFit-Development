32e70679e989ed43e9dd519dd991b10bdbfa418e
“Harsh09-11” <harshsharma889@gmail.com>
2024-01-25 11:33:26 +0530

    booking dao and service implementation

--- JEDI-Flipfit-JAVA-POS/src/com/flipkart/dao/BookingDAO.java
+++ JEDI-Flipfit-JAVA-POS/src/com/flipkart/dao/BookingDAO.java
@@ -6,24 +6,30 @@ package com.flipkart.dao;
 import com.flipkart.bean.Booking;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 
  */
 public interface BookingDAO {
-    public boolean insertBooking(Booking booking);
+    public boolean insertBooking(int userId,int gymID,int slotId);
     /**
      * Remove the booking from the database
      * @param slotID
      * @param customerID
      * @return boolean value whether the removal was successful or not
      */
-    public boolean removeBooking(int slotID, int customerID);
+    public boolean cancelBooking(int bookingId);
     /**
      * Get a list of all of the bookings of a user
      * @param customerID
      * @return List containing all the bookings of a user
      */
-    public ArrayList<Booking> viewBookings(int customerID);
+
+    Booking getBooking(int bookingId);
+    List<Booking> getBookingsByUserId(int userId);
+    List<Booking> getBookingsByGymId(int gymId);
+    List<Booking> getBookingsBySlotId(int slotId);
+    List<Booking> getAllBookings();
 
 }
--- JEDI-Flipfit-JAVA-POS/src/com/flipkart/service/BookingFlipFitService.java
+++ JEDI-Flipfit-JAVA-POS/src/com/flipkart/service/BookingFlipFitService.java
@@ -1,13 +1,23 @@
 package com.flipkart.service;
 
+import com.flipkart.bean.Booking;
 import com.flipkart.bean.BookingDetails;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public interface BookingFlipFitService {
 	
-	boolean updateBooking(int bookingId);
+	boolean insertBooking(int userId,int gymID,int slotId);
 	
 	boolean cancelBooking(int bookingId);
 	
-	BookingDetails getBookingDetails(int bookingId);
+	Booking getBooking(int bookingId);
+	List<Booking> getBookingsByUserId(int userId);
+	List<Booking> getBookingsByGymId(int gymId);
+	List<Booking> getBookingsBySlotId(int slotId);
+	List<Booking> getAllBookings();
+
+
 
 }

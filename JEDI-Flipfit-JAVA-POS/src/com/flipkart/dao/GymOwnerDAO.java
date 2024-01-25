11a4ea50b83f31336cd62da8fc8b6a1019d34991
deepakguptaflipkart <deepak.vinodgupta@flipkart.com>
2024-01-25 12:22:07 +0530

    feat: gym owner dao implementation completed

--- JEDI-Flipfit-JAVA-POS/src/com/flipkart/dao/GymOwnerDAO.java
+++ JEDI-Flipfit-JAVA-POS/src/com/flipkart/dao/GymOwnerDAO.java
@@ -12,66 +12,6 @@ import java.util.List;
 public interface GymOwnerDAO {
 
     /**
-     * Adds a new gym to the database.
-     *
-     * @param gym The Gym object containing gym details.
-     */
-    void addGym(Gym gym);
-
-    /**
-     * Retrieves all gyms associated with a specific GymOwner.
-     *
-     * @param userId The user ID of the GymOwner.
-     * @return List of Gym objects associated with the GymOwner.
-     */
-    List<Gym> getAllGyms(int userId);
-
-    /**
-     * Removes a gym based on its gym ID.
-     *
-     * @param gymId The ID of the gym to be removed.
-     * @return True if the gym is successfully removed, false otherwise.
-     */
-    boolean removeGym(int gymId);
-
-    /**
-     * Modifies gym details based on its gym ID.
-     *
-     * @param gymId The ID of the gym to be modified.
-     * @return True if the gym is successfully modified, false otherwise.
-     */
-    boolean modifyGym(int gymId);
-
-    /**
-     * Adds a new slot for the specified gym.
-     *
-     * @param gym The Gym object for which the slot needs to be added.
-     */
-    void addSlot(Gym gym);
-
-    /**
-     * Modifies an existing slot for the specified gym.
-     *
-     * @param gymId The ID of the gym for which the slot needs to be modified.
-     */
-    void modifySlot(int gymId);
-
-    /**
-     * Retrieves the list of slots for a specific gym.
-     *
-     * @param gymId The ID of the gym for which slots need to be retrieved.
-     * @return List of strings representing slots for the specified gym.
-     */
-    List<String> viewSlots(int gymId);
-
-    /**
-     * Removes a slot for the specified gym.
-     *
-     * @param gymId The ID of the gym for which the slot needs to be removed.
-     */
-    void removeSlot(int gymId);
-
-    /**
      * Retrieves the profile details of the GymOwner.
      *
      * @param user The User object representing the GymOwner.
--- JEDI-Flipfit-JAVA-POS/src/com/flipkart/dao/daoImpl/GymOwnerDAOImpl.java
+++ JEDI-Flipfit-JAVA-POS/src/com/flipkart/dao/daoImpl/GymOwnerDAOImpl.java
@@ -7,19 +7,19 @@ import com.flipkart.bean.Gym;
 import com.flipkart.bean.GymOwner;
 import com.flipkart.bean.User;
 import com.flipkart.dao.GymOwnerDAO;
+import com.flipkart.service.GymFlipFitService.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class GymOwnerDAOImpl implements GymOwnerDAO {
 
-    ArrayList<Gym> gyms = new ArrayList<Gym>();
-
+    ArrayList<GymOwner> gymOwners = new ArrayList<GymOwner>();
     private static GymOwnerDAOImpl gymOwnerDaoObj = null;
 
     private GymOwnerDAOImpl() {
         // Initialize the gyms list or perform any necessary setup
-        gyms = new ArrayList<>();
+        ArrayList<Gym> gyms = new ArrayList<>();
     }
 
     public static synchronized GymOwnerDAOImpl getInstance() {
@@ -31,60 +31,25 @@ public class GymOwnerDAOImpl implements GymOwnerDAO {
 
 
     @Override
-    public void addGym(Gym gym) {
-        gyms.add(gym);  // Add the gym to the list
-        // Add logic to persist the gym details in the database
-
-    }
-
-    @Override
-    public List<Gym> getAllGyms(int userId) {
-        // Return the list of gyms associated with the GymOwner identified by userId
-        return gyms;
-    }
-
-    @Override
-    public boolean removeGym(int gymId) {
-        // Add logic to remove the gym based on gymId
-        return false;
-    }
-
-    @Override
-    public boolean modifyGym(int gymId) {
-        // Add logic to modify the gym based on gymId
-        return false;
-    }
-
-    @Override
-    public void addSlot(Gym gym) {
-        // Add logic to add a slot for the specified gym
-    }
-
-    @Override
-    public void modifySlot(int gymId) {
-        // Add logic to modify a slot for the specified gym based on gymId
-    }
-
-    @Override
-    public List<String> viewSlots(int gymId) {
-        // Add logic to retrieve and return the list of slots for the specified gym based on gymId
-        return null;
-    }
-
-    @Override
-    public void removeSlot(int gymId) {
-        // Add logic to remove a slot for the specified gym based on gymId
-    }
-
-    @Override
     public GymOwner viewProfile(User user) {
         // Add logic to retrieve and return the profile details of the GymOwner based on the User object
+        for (GymOwner owner : gymOwners) {
+            if (owner.getUsername().equals(user.getUsername())) {
+                return owner;
+            }
+        }
         return null;
     }
 
     @Override
     public GymOwner updateProfile(GymOwner owner) {
         // Add logic to update the profile details of the GymOwner based on the GymOwner object
-        return owner;
+        for (GymOwner o : gymOwners) {
+            if (o.getUsername().equals(owner.getUsername())) {
+                o = owner;
+                return o;
+            }
+        }
+        return null;
     }
 }

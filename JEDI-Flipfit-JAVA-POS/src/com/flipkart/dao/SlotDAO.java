ac63bfeba668aeebd8fcd92d644c1bc2983bf89f
sakshi-s0ni <sakshisoni61000@gmail.com>
2024-01-25 10:26:55 +0530

    Slot Dao

--- JEDI-Flipfit-JAVA-POS/src/com/flipkart/dao/SlotDAO.java
+++ JEDI-Flipfit-JAVA-POS/src/com/flipkart/dao/SlotDAO.java
@@ -3,9 +3,21 @@
  */
 package com.flipkart.dao;
 
+import com.flipkart.bean.Slot;
+
+import java.util.ArrayList;
+
 /**
  * 
  */
 public interface SlotDAO {
+    boolean isAvailable();
 
+
+    boolean addSlot(int slotHour, int gymID, int availableSeats);
+
+    boolean updateSlot(int slotHour, int gymID, int changeInSeats);
+
+
+    ArrayList<Slot> getAllAvailableSlots();
 }
--- JEDI-Flipfit-JAVA-POS/src/com/flipkart/dao/daoImpl/SlotDAOImpl.java
+++ JEDI-Flipfit-JAVA-POS/src/com/flipkart/dao/daoImpl/SlotDAOImpl.java
@@ -3,9 +3,37 @@
  */
 package com.flipkart.dao.daoImpl;
 
+import com.flipkart.bean.Slot;
+import com.flipkart.dao.SlotDAO;
+
+import java.util.ArrayList;
+
 /**
  * 
  */
-public class SlotDAOImpl {
+public class SlotDAOImpl implements SlotDAO {
+    @Override
+    public boolean isAvailable() {
+        return false;
+    }
 
+
+    @Override
+    public boolean addSlot(int slotHour, int gymID, int availableSeats) {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean updateSlot(int slotHour, int gymID, int changeInSeats) {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+
+    @Override
+    public ArrayList<Slot> getAllAvailableSlots() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }

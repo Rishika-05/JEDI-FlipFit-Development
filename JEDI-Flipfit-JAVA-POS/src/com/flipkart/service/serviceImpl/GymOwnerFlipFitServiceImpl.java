213f2c31e747396fe4cf0d6ca98f3db12b279287
deepakguptaflipkart <deepak.vinodgupta@flipkart.com>
2024-01-25 12:42:15 +0530

    feat: gym owmer services implementation

--- JEDI-Flipfit-JAVA-POS/src/com/flipkart/service/serviceImpl/GymOwnerFlipFitServiceImpl.java
+++ JEDI-Flipfit-JAVA-POS/src/com/flipkart/service/serviceImpl/GymOwnerFlipFitServiceImpl.java
@@ -3,33 +3,36 @@
  */
 package com.flipkart.service.serviceImpl;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import com.flipkart.bean.Gym;
 import com.flipkart.bean.GymOwner;
 import com.flipkart.bean.User;
+import com.flipkart.dao.daoImpl.GymOwnerDAOImpl;
 import com.flipkart.service.GymOwnerFlipFitService;
 
 /**
  * 
  */
 public class GymOwnerFlipFitServiceImpl implements GymOwnerFlipFitService {
-
+	List<Gym> gyms = new ArrayList<Gym>();
 	@Override
-	public void addGym(Gym gmy) {
+	public void addGym(Gym gym) {
 		// TODO Auto-generated method stub
-		
+		gyms.add(gym);
 	}
 
 	@Override
 	public List<Gym> getAllGyms(int userId) {
 		// TODO Auto-generated method stub
-		return null;
+		return gyms;
 	}
 	@Override
 	public void addTimeSlot(Gym gym) {
 		//logic to add time slot
-		return;
+		
 	}
 	
 
@@ -38,14 +41,42 @@ public class GymOwnerFlipFitServiceImpl implements GymOwnerFlipFitService {
 	public GymOwner viewProfile(User user) {
 		//Get gym owner profile from username and password
 		
-		GymOwner gymOwnerProfile = new GymOwner();
-		return gymOwnerProfile;
+		return GymOwnerDAOImpl.getInstance().viewProfile(user);
 	}
 	
 	@Override
 	public GymOwner updateProfile(GymOwner gymOwner) {
 		//get details and update accordingly
-		
-		return gymOwner;
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Password: ");
+		String password = sc.next();
+		gymOwner.setPassword(password);
+
+		System.out.println("Pin Code: ");
+		String pinCode = sc.next();
+		gymOwner.setPincode(pinCode);
+
+		System.out.println("Age: ");
+		int age = sc.nextInt();
+		gymOwner.setAge(age);
+
+		System.out.println("Pan Card: ");
+		String panCard = sc.next();
+		gymOwner.setPanCard(panCard);
+
+		System.out.println("Aadhar Card: ");
+		String aadhar = sc.next();
+		gymOwner.setAadharCard(aadhar);
+
+		System.out.println("GST IN: ");
+		String gst = sc.next();
+		gymOwner.setGstin(gst);
+
+		System.out.println("Address: ");
+		String address = sc.next();
+		gymOwner.setAddress(address);
+
+		return GymOwnerDAOImpl.getInstance().updateProfile(gymOwner);
 	}
 }

cdf65b7b0e9c550b5be36be72113fa6b66aa3091
deepakguptaflipkart <deepak.vinodgupta@flipkart.com>
2024-01-25 11:24:07 +0530

    fix: gym owner dao attribute

--- JEDI-Flipfit-JAVA-POS/src/com/flipkart/dao/GymOwnerDAO.java
+++ JEDI-Flipfit-JAVA-POS/src/com/flipkart/dao/GymOwnerDAO.java
@@ -15,8 +15,12 @@ import java.util.List;
 public interface GymOwnerDAO {
     void addGym(Gym gym);
     List<Gym> getAllGyms(int userId);
-
-    void addTimeSlot(Gym gym);
+    boolean removeGym(int gymId);
+    boolean modifyGym(int gymId);
+    void addSlot(Gym gym);
+    void modifySlot(int gymId);
+    List<String> viewSlots(int gymId);
+    void removeSlot(int gymId);
 
     GymOwner viewProfile(User user);
     GymOwner updateProfile(GymOwner owner);

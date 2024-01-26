9dbc932d3d1dfbfa504029d90765fe674f55d521
Rishika-05 <rishikaraj7263@gmail.com>
2024-01-26 10:02:48 -0800

    fix: connection

--- JEDI-Flipfit-JAVA-POS/src/com/flipkart/dao/DBConnection.java
+++ JEDI-Flipfit-JAVA-POS/src/com/flipkart/dao/DBConnection.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 
 public class DBConnection {
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	static final String DB_URL = "jdbc:mysql://localhost/FlipfitSchema";
+	static final String DB_URL = "jdbc:mysql://localhost/flipfit_schema";
 
 	static final String USER = "root";
 

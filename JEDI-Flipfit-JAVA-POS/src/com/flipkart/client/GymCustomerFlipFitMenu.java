6dd9de23b111e6a0147e4cbb2428e20041948a75
sakshi-s0ni <sakshisoni61000@gmail.com>
2024-01-28 18:39:57 +0530

    fix : unnecessary console prints removed

--- JEDI-Flipfit-JAVA-POS/src/com/flipkart/client/GymCustomerFlipFitMenu.java
+++ JEDI-Flipfit-JAVA-POS/src/com/flipkart/client/GymCustomerFlipFitMenu.java
@@ -215,7 +215,7 @@ public class GymCustomerFlipFitMenu {
 
 
 	    private void bookSlot(Scanner sc, int userId) {
-	    	GymCustomer gymCustomer = customerService.viewProfile(userId);
+//	    	GymCustomer gymCustomer = customerService.viewProfile(userId);
 	    	 // Ask the user to select a location
 		    System.out.println("\n\033[1mSelect a location:\033[0m");
 		    System.out.println("1. Bellandur");

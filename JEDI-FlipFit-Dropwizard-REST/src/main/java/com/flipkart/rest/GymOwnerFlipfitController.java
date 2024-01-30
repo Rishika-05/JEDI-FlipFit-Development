3d9fa904e6abb5afd2511afd670904d2489ca42b
Rishika-05 <rishikaraj7263@gmail.com>
2024-01-30 21:46:57 +0530

    feat: add controller

--- JEDI-FlipFit-Dropwizard-REST/src/main/java/com/flipkart/rest/GymOwnerFlipfitController.java
+++ JEDI-FlipFit-Dropwizard-REST/src/main/java/com/flipkart/rest/GymOwnerFlipfitController.java
@@ -4,6 +4,7 @@
 package com.flipkart.rest;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -15,5 +16,8 @@ import javax.ws.rs.core.MediaType;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class GymOwnerFlipfitController {
-
+	 @GET
+	    public String getEmployees() {
+	        return "my dropwizard service";
+	    }
 }

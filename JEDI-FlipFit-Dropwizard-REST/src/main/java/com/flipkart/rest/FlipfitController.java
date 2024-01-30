c28782a59b4d2da468091a5eb5a4d28132dfcc67
Rishika-05 <rishikaraj7263@gmail.com>
2024-01-30 17:12:09 +0530

    feat: add controller

---
+++ JEDI-FlipFit-Dropwizard-REST/src/main/java/com/flipkart/rest/FlipfitController.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.rest;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * 
+ */
+@Path("/hello")
+@Produces(MediaType.APPLICATION_JSON)
+public class FlipfitController {
+	@GET
+    public String getEmployees() {
+        return "my dropwizard service";
+    }
+}
